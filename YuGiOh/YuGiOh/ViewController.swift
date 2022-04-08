29bae1c7f4809fd8f08f94e6b7dc5119172d21f5
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2022-04-08 19:25:42 +0400

    feat: add project

---
+++ YuGiOh/YuGiOh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YuGiOh
+//
+//  Created by Evgeniy Petlitskiy on 8.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
