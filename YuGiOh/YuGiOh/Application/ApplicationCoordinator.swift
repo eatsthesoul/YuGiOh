25d5db46eb9d5d483fcb1dc7c9cc9f2ff7fae6be
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2022-04-11 16:04:13 +0400

    feat: add ApplicationCoordinator

---
+++ YuGiOh/YuGiOh/Application/ApplicationCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  ApplicationCoordinator.swift
+//  YuGiOh
+//
+//  Created by Evgeniy Petlitskiy on 11.04.22.
+//
+
+import Foundation
+
+final class ApplicationCoordinator: BaseCoordinator {
+
+    // MARK: - Coordinator
+
+    override func start() {
+        runMainFlow()
+    }
+}
+
+// MARK: - Private Methods
+
+private extension ApplicationCoordinator {
+
+    func runMainFlow() {
+        
+    }
+}
